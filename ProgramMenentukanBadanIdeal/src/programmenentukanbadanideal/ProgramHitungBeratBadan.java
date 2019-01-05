2781437b7aee463afb9837c0d2e5dbcc87aa1109
grgsersan109 <45782620+grgsersan109@users.noreply.github.com>
2019-01-05 16:14:19 +0700

    Starting The Project

---
+++ ProgramMenentukanBadanIdeal/src/programmenentukanbadanideal/ProgramHitungBeratBadan.java
@@ -0,0 +1,11 @@
+package programmenentukanbadanideal;
+
+public class ProgramHitungBeratBadan {
+
+    public static void main(String[] args) {
+                
+        MainMenu obj = new MainMenu();
+        obj.setVisible(true);
+        
+    }
+}
